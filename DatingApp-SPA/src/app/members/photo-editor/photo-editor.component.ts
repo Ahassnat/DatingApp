c95e0a4c5ae1e8a218254c30e0007f31d36cf748
Abdallah <alhassnatabdallah@gmail.com>
2019-11-24 18:29:43 +0200

    Configuring the 3rd Party file uploader

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -14,6 +14,7 @@ export class PhotoEditorComponent implements OnInit {
   uploader: FileUploader;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
+
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
@@ -33,6 +34,23 @@ export class PhotoEditorComponent implements OnInit {
     autoUpload: false,
     maxFileSize: 10 * 1024 * 1024
   });
+  // onAfterAddingFile this function to make the authToken Secure
+  this.uploader.onAfterAddingFile = (file) => {file.withCredentials = false; };
+
+  // onSuccessItem this fucction to show the result of uploading the photo immedately without refreshing the page
+  this.uploader.onSuccessItem = (item, response, status, headers) => {
+    if (response) {
+      const res: Photo = JSON.parse(response); // convetr the string to json file
+      const photo = {
+        id: res.id,
+        url: res.url,
+        dateAdded: res.dateAdded,
+        description: res.description,
+        isMain: res.isMain
+      };
+      this.photos.push(photo);
+    }
+  };
  }
 
 }
