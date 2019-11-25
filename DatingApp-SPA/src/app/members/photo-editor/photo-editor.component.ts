e14e5c0d45bf4a2ce0160f8fb8e1023bc16f02b5
Abdallah <alhassnatabdallah@gmail.com>
2019-11-25 23:01:25 +0200

    Using array filter to provide instant in the SPA

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -16,6 +16,7 @@ export class PhotoEditorComponent implements OnInit {
   uploader: FileUploader;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
+  currentMain: Photo;
 
   constructor(private authService: AuthService, private userService: UserService, private toastrService: ToastrService) { }
 
@@ -57,7 +58,10 @@ export class PhotoEditorComponent implements OnInit {
 
  setMainPhoto(photo: Photo) {
   this.userService.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
-    console.log('success set the main photo');
+    this.currentMain = this.photos.filter(p => p.isMain === true)[0];
+    this.currentMain.isMain = false;
+    photo.isMain = true;
+    // console.log('success set the main photo');
   }, error => {
     this.toastrService.error(' Error', 'Photo Setting ERROR');
   });
