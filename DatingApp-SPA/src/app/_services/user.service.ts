c906e18f173f8151567c2cefbebf039cf537b9b5
Abdallah <alhassnatabdallah@gmail.com>
2019-12-04 00:14:32 +0200

    12. Adding the Sorting functionality to the SPA

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -39,6 +39,7 @@ getUsers(page?, itemsPerPage?, userParams?): Observable<PaginatedResult<User[]>>
     params = params.append('minAge', userParams.minAge);
     params = params.append('maxAge', userParams.maxAge);
     params = params.append('gender', userParams.gender);
+    params = params.append('orderBy', userParams.orderBy);
   }
 
   return this.http.get<User[]>(this.baseUrl + 'users', {observe: 'response', params})
