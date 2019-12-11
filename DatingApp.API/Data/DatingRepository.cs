89ab5453774e3e8a918a2c8d767660685da15ced
Abdallah <alhassnatabdallah@gmail.com>
2019-12-11 23:27:04 +0200

    Adding the Message thread methods to the API

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -144,9 +144,16 @@ namespace DatingApp.API.Data
                          messageParams.PageNumber, messageParams.PageSize);
         }
 
-        public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+        public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
         {
-            throw new NotImplementedException();
+            var messages = await _context.Messages
+                                .Include(u => u.Sender).ThenInclude(p => p.Photos)
+                                .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+                                .Where(m => m.RecipientId == userId && m.SenderId == recipientId
+                                     || m.RecipientId == recipientId && m.SenderId == userId)
+                                .OrderByDescending(m => m.MessageSent).ToListAsync();
+
+            return messages;
         }
     }
 }
